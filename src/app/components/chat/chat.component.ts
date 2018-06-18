86b4f7fde6f25b9442531b3a2966d9598f8d8d20
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-06-18 19:11:12 -0400

    Logic to get chat messages implemented

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import 'rxjs/add/operator/takeUntil';
 
 import { SubscriptionsService } from '../../services/subscriptions/subscriptions.service';
+import { ChatService } from '../../services/chat/chat.service';
+import { Ichat } from '../../interfaces/ichat';
 
 const noPhotoURL: string = './assets/img/nophoto.png';
 
@@ -15,22 +17,45 @@ export class ChatComponent implements OnInit {
 
   noPhotoURL: string;
 
+  chatRoomId: string;
+
   restaurantId: string;
 
   userId: string;
 
-  constructor(private route: ActivatedRoute,
+  messages: Ichat[];
+
+  constructor(private chatService: ChatService,
+    private route: ActivatedRoute,
     private router: Router,
     private subscriptions: SubscriptionsService) {
 
     this.noPhotoURL = noPhotoURL;
+    this.messages = [];
   }
 
   ngOnInit() {
     this.route.params.takeUntil(this.subscriptions.unsubscribe).subscribe(
       (params) => {
-        let chatRoomId: string = params['id'];
-        // chatRoomId.
+        this.chatRoomId = params['id'];
+        let backSlashIndex: number = this.chatRoomId.indexOf('_');
+
+        this.restaurantId = this.chatRoomId.substring(0, backSlashIndex);
+        this.userId = this.chatRoomId.substring(backSlashIndex + 1, this.chatRoomId.length);
+
+        console.log(this.restaurantId);
+        console.log(this.userId);
+      }
+    );
+  }
+
+  private setChatRoomMessages(): void {
+    this.chatService.getChatsByChatRoomId(this.chatRoomId).subscribe(
+      messages => {
+        this.messages = messages;
+      },
+      error => {
+        console.error(error);
       }
     );
   }
