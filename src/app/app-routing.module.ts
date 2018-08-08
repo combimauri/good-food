2131a17a728df717ab2cadbcbe5e278cb5ab0a0f
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-08-08 18:34:45 -0400

    App icons modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,7 +81,7 @@ const routes: Routes = [
     {
         path: 'offline',
         component: OfflineComponent,
-        // canActivate: [AuthenticationGuardService]
+        canActivate: [AuthenticationGuardService]
     },
     {
         path: '**',
--- src/app/services/subscriptions/subscriptions.service.ts
+++ src/app/services/subscriptions/subscriptions.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { ISubscription } from 'rxjs/Subscription';
 import { Subject } from 'rxjs/Subject';
 
 @Injectable()
