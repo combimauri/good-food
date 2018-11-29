14cf26aa50e629a2a93a64e52fd3620424555019
Mauricio Arce <Mauricio.Arce@communitybrands.com>
2018-11-29 18:20:43 -0400

    Firebase hosting configured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAkApZBHn5v1SBdeWhfUAJkxacy05nr_Y0",
-    authDomain: "good-food-6e2a9.firebaseapp.com",
-    databaseURL: "https://good-food-6e2a9.firebaseio.com",
-    projectId: "good-food-6e2a9",
-    storageBucket: "good-food-6e2a9.appspot.com",
-    messagingSenderId: "249999816376"
-  },
-  googlemaps: 'AIzaSyAYVqWMdgwJUIKviVoxpcWfTtGTgWN6LYU'
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyAkApZBHn5v1SBdeWhfUAJkxacy05nr_Y0',
+        authDomain: 'good-food-6e2a9.firebaseapp.com',
+        databaseURL: 'https://good-food-6e2a9.firebaseio.com',
+        projectId: 'good-food-6e2a9',
+        storageBucket: 'good-food-6e2a9.appspot.com',
+        messagingSenderId: '249999816376'
+    },
+    googlemaps: 'AIzaSyAYVqWMdgwJUIKviVoxpcWfTtGTgWN6LYU'
 };
